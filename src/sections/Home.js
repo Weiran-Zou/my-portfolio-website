db40b88e5597fe8d1da0317720c0d5efe8a2ed44
Weiran Zou <zouweiran9122@gmail.com>
2024-09-26 12:23:32 +1000

    fix bug

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -19,64 +19,65 @@ export default function Home() {
     const color = useMotionValue(COLORS_TOP[0]);
 
     const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
-
+    useEffect(() => {
+        animate(color, COLORS_TOP, {
+          ease: "easeInOut",
+          duration: 10,
+          repeat: Infinity,
+          repeatType: "mirror",
+        });
+      }, []);
     return (
-        <motion.div id="home"  style={{
-            backgroundImage,
-            padding:"5rem"
-          }} >
-            
-                <div id="intro-text">
-                    <div id="intro-type">
-                        <TypeAnimation
-                            preRenderFirstString={true}
-                            sequence={[
-                                "Hi, I'm Weiran (Peter) Zou. \n",
-                                1000,
-                                "Hi, I'm Weiran (Peter) Zou. \n Full Stack Development",
-                                1000,
-                                "Hi, I'm Weiran (Peter) Zou. \n Android App Development",
-                                1000
-                                
-                            ]}
-                            wrapper="span"
-                            speed={50}
-                            style={{whiteSpace: 'pre-line', fontWeight:"bold" }}
-                            repeat={Infinity}
-                        />
-                    
-                    </div>
-                    <div id="intro-descp">  
-                        <motion.p 
-                            initial={{x:-100, opacity:0}}
-                            animate={{x:0, opacity:1}}
-                            transition={{duration: 0.5, delay: 0.5}}
-                            
-                        >
-                            I am an enthusiastic and self-motivated young professional with a passion for software development and creating innovative digital solutions. I am eager to join collaborative environment where I can leverage my skills and contribute to impactful projects.
-                        </motion.p>   
-                        
-                    </div>
-                </div>
-                
-                <div id="intro-img">
-                    <motion.img 
-                        style={{width:"100%",height:"auto"}}
-                        initial={{x:200, opacity:0}}
-                        animate={{x:0, opacity:1}}
-                        transition={{duration: 0.5, delay: 0.5}} src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c83c004e-1370-4756-88e5-4071de797088/dgdq8br-09cc7ad6-a021-47a5-b0e0-917b12b0f7a7.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M4M2MwMDRlLTEzNzAtNDc1Ni04OGU1LTQwNzFkZTc5NzA4OFwvZGdkcThici0wOWNjN2FkNi1hMDIxLTQ3YTUtYjBlMC05MTdiMTJiMGY3YTcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.tqRMtE-b2QiI2nnefNxSDMJvZCcYqFmq2ccg_Xfzqb8" alt='profile'
-                    />  
-                </div>
-               
+        <motion.div 
+            id="home"  
+            style={{
+                backgroundImage,
+                width: "100%",
+                height: "100vh"
+            }}
+        >
         
-                {/* <ReactTyped 
-                    className='typing'              
-                    strings={["Hi, I'm Weiran Zou. <br/> Aspiring software developer"]} 
-                    typeSpeed={40}
-                />                           */}
-              
-           
+            <div id="intro-text">
+                <div id="intro-type">
+                    <TypeAnimation
+                        preRenderFirstString={true}
+                        sequence={[
+                            "Hi, I'm Weiran (Peter) Zou. \n",
+                            1000,
+                            "Hi, I'm Weiran (Peter) Zou. \n Full Stack Development",
+                            1000,
+                            "Hi, I'm Weiran (Peter) Zou. \n Android App Development",
+                            1000
+                            
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{whiteSpace: 'pre-line', fontWeight:"bold" }}
+                        repeat={Infinity}
+                    />
                 
+                </div>
+                <div id="intro-descp">  
+                    <motion.p 
+                        initial={{x:-100, opacity:0}}
+                        animate={{x:0, opacity:1}}
+                        transition={{duration: 0.5, delay: 0.5}}
+                        
+                    >
+                        I am an enthusiastic and self-motivated young professional with a passion for software development and creating innovative digital solutions. I am eager to join collaborative environment where I can leverage my skills and contribute to impactful projects.
+                    </motion.p>   
+                    
+                </div>
+            </div>
+            
+            <div id="intro-img">
+                <motion.img 
+                    
+                    initial={{x:200, opacity:0}}
+                    animate={{x:0, opacity:1}}
+                    transition={{duration: 0.5, delay: 0.5}} src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c83c004e-1370-4756-88e5-4071de797088/dgdq8br-09cc7ad6-a021-47a5-b0e0-917b12b0f7a7.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M4M2MwMDRlLTEzNzAtNDc1Ni04OGU1LTQwNzFkZTc5NzA4OFwvZGdkcThici0wOWNjN2FkNi1hMDIxLTQ3YTUtYjBlMC05MTdiMTJiMGY3YTcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.tqRMtE-b2QiI2nnefNxSDMJvZCcYqFmq2ccg_Xfzqb8" alt='profile'
+                />  
+            </div>
               
             <div style={{position:"fixed", zIndex: "0px", inset: "0px"}}>
                 <Canvas>
