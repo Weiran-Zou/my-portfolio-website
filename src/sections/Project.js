43b08b20ca1e5512561f8a254ca96c8181d75551
Weiran Zou <zouweiran9122@gmail.com>
2024-09-26 11:37:35 +1000

    add project card hover animation

--- src/sections/Project.js
+++ src/sections/Project.js
@@ -1,9 +1,4 @@
-import { 
-    motion, 
-    useMotionTemplate,
-    useMotionValue,
-    useSpring 
-} from "framer-motion"
+
 import React from "react";
 import { useRef, useEffect, useState } from "react"
 import { Link } from "react-router-dom";
@@ -17,38 +12,7 @@ const HALF_ROTATION_RANGE = 32.5 / 2;
 
 const Project = ({item}) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const ref = useRef(null);
 
-    const x = useMotionValue(0);
-    const y = useMotionValue(0);
-
-    const xSpring = useSpring(x);
-    const ySpring = useSpring(y);
-
-    const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
-
-    const handleMouseMove = (e) => {
-        if (!ref.current) return [0, 0];
-
-        const rect = ref.current.getBoundingClientRect();
-
-        const width = rect.width;
-        const height = rect.height;
-
-        const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
-        const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
-
-        const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
-        const rY = mouseX / width - HALF_ROTATION_RANGE;
-
-        x.set(rX);
-        y.set(rY);
-    };
-
-    const handleMouseLeave = () => {
-        x.set(0);
-        y.set(0);
-    };
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -83,31 +47,9 @@ const Project = ({item}) => {
                 
                
             </MyModal>
-            <motion.div 
-                
-                className="project-card"
-                variants={{hidden: {opacity: 0}, show: {opacity: 1}}}
-                whileHover={{
-                    // rotate: 5,
-                    scale:1.05,
-                    transition:{duration:0.8}
-                }}
-                // whileHover={{ scale: 0.95, rotate: "-1deg" }}
-                ref={ref}
-                onMouseMove={handleMouseMove}
-                onMouseLeave={handleMouseLeave}
-                style={{
-                    transformStyle: "preserve-3d",
-                    transform,
-                }}
-                >
-                <img src={item.img}  alt="..." style={{
-                    transform: "translateZ(75px)",
-                    transformStyle: "preserve-3d",
-                }}/>
-                <div className="card-body"  style={{
-                    transform: "translateZ(50px)",
-                }}>
+            <div className="project-card">
+                <img src={item.img}  alt="..." />
+                <div className="card-body"  >
                     <h5 className="card-title">{item.title}</h5>
                     <ul className="techList">
                         { item.tech.map(tech =>
@@ -116,9 +58,11 @@ const Project = ({item}) => {
                         
                     </ul>
                     <p className="card-text">{item.description}</p>
+                </div>
+                <div className="overlay">
                     <button type="button" class="btn btn-primary" onClick={openModal}>Show More</button>
                 </div>
-            </motion.div>
+            </div>
         </>
         // </Link>
        
