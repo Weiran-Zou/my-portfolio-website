1e8750075940e432235908deffa377fd2cb25864
Weiran Zou <zouweiran9122@gmail.com>
2024-10-18 00:40:51 +1100

    Add flip animation to skill cards on hover

--- src/components/Skillcard.js
+++ src/components/Skillcard.js
@@ -1,35 +1,11 @@
 import "./Skillcard.css"
-import { motion} from "framer-motion"
 
-const iconVariant = (duration) => ({
-    initial: {y: -10},
-    animate: {
-        y: [10, -10],
-        transition: {
-            duration: duration,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "reverse"
-        }
-    }
-})
 export default function Skillcard(props) {
     return (
-        <motion.div  
-            whileHover={{
-                scale: 1.1,
-                duration: 0.8,
-                
-            }}
-            variants={iconVariant(2.5)}
-            initial="initial"
-            animate="animate"
-            id="skill-card"
-        >
-            
-            <img src={props.src} alt="..."></img>
-
-        </motion.div >
+        <div id="skill-card">
+            <img className="skill-card__front" src={props.src} alt="..."></img>
+            <div className="skill-card__back"> {props.skillName}</div>
+        </div >
     
     )
 
